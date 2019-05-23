3990b788bd754251ffc9c8c33d5fa98cd69da7f3
birna <briandam26@yahoo.com>
2019-05-22 19:02:14 -0700

    pseudocoded - divided up codework a bit prior to coding on game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+//Display instructions
+
+//Create word bank that gets randomly picked
+//-Array containing several words (strings) and then shuffle 
+
+//Present blanks for users to guess
+//-The letters from the string will be used to cross-reference
+//with the user's key entries 
+
+//Following each guess, subtract from amount of guesses given
+
+//Refresh the blank spaces with correct letters
+
+//Display the letters user have already guessed on the side
+
+//If user runs out of guesses, alert loss and reset the game
+
+//If user guesses correctly, play a sound, 
+//tally up win, and reset the game
+
+
+
