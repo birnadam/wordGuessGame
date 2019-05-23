4293a9214c1341e42424dbfa81ac280d3d2683fa
birna <briandam26@yahoo.com>
2019-05-22 21:27:33 -0700

    created first two pages, added two functions to js including some variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,24 @@
 //Display instructions
+function sp(){
+    document.getElementById('introPage').style.display = "none";
+    document.getElementById('infoPage').style.display = "block";
+}
 
 //Create word bank that gets randomly picked
 //-word bank (strings in an array):
 const words = ["unicorn", "dragon", "mermaid", "fairy", "werewolf", "leprechaun", "sphinx", "griffin", "pegasus", "chimera",
 "centaur", "ghoul", "troll", "gnome", "pixie", "phoenix"];
 
-//-Array containing several words (strings) and then shuffle 
+//-Once user begins, the word is shuffled
+//-create placement lets to do so:
+let rand = 0;
+let word = "";
+
+function okay(){
+    rand = Math.floor(Math.random()*words.length);
+    word = words[rand];
+    document.getElementById('infoPage').style.display = "none";
+}
 
 //Present blanks for users to guess
 //-The letters from the string will be used to cross-reference
